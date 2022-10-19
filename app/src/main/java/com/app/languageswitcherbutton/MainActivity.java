201240519baa1f8c77a36aa2ce1cbfa6e1fb5550
Raufun <raufun.nazin13@gmail.com>
2022-10-19 12:10:24 +0600

    created Locale and updated MainActivity

--- app/src/main/java/com/app/languageswitcherbutton/MainActivity.java
+++ app/src/main/java/com/app/languageswitcherbutton/MainActivity.java
@@ -1,14 +1,42 @@
 package com.app.languageswitcherbutton;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView messageView;
+    Button btnBangla, btnEnglish;
+    Context context;
+    Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // referencing the text and button views
+        messageView = (TextView) findViewById(R.id.textView);
+        btnBangla = findViewById(R.id.btnBangla);
+        btnEnglish = findViewById(R.id.btnEnglish);
+
+        // setting up on click listener event over the button
+        // in order to change the language with the help of
+        // LocaleHelper class
+        btnEnglish.setOnClickListener(view -> {
+            context = LocaleHelper.setLocale(MainActivity.this, "en");
+            resources = context.getResources();
+            messageView.setText(resources.getString(R.string.language));
+        });
+
+        btnBangla.setOnClickListener(view -> {
+            context = LocaleHelper.setLocale(MainActivity.this, "hi");
+            resources = context.getResources();
+            messageView.setText(resources.getString(R.string.language));
+        });
+
     }
 }
